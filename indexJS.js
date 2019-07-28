983a893af16b8303149eaa19bcd17abf224104ba
moueza <mouezapeter@gmail.com>
2019-07-28 21:54:07 +0200

    KO deprecated getCSSCanvasContext

--- indexJS.js
+++ indexJS.js
@@ -1,5 +1,51 @@
+function deletee() {
+    document.getElementById("myCanvas").style.color = "white";
 
-document.getElementById("demo").innerHTML = "Paragraph changed.";
-        function myFunction() {
- document.getElementById("demo").innerHTML = "Paragraph changed.";
+
+    var c = document.getElementById("myCanvas");
+    var ctx = c.getContext("2d");
+
+//4 times because alpha just decreases 4 times
+for (i = 0; i <= 3; i++) {
+     ctx.strokeStyle = "#FFFFFF";
+    ctx.moveTo(0, 0);
+    ctx.lineTo(200, 100);
+    ctx.stroke();
 }
+   
+
+}
+var x = document.getElementById("myAudio");
+
+
+function playsound() {
+    x.play();
+
+
+}
+
+
+
+
+function stopsound() {
+    x.pause();
+
+
+}
+
+
+
+function draww() {
+    document.getElementById("demotxt").innerHTML = "Paragraph changed.";
+
+
+    var c = document.getElementById("myCanvas");
+  //  var ctx = c.getContext("2d");
+   var ctx =document.getCSSCanvasContext("2d", "mybackground", 100, 100);/*https://filosophy.org/code/using-html5-canvas-to-make-a-generative-background/*/  ctx.strokeStyle = "#000000";
+    ctx.moveTo(0, 0);
+    ctx.lineTo(200, 100);
+    ctx.stroke();
+}
+
+
+draww();
